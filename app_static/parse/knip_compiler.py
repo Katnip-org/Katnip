e8af994c4eab0d61e5b8c0deafb7e9be14053219
Benjamin Clark <benclarkbobb@gmail.com>
2025-07-18 00:11:54 -0400

    Cleaned branch for svelte project creation. Overhaul start

--- app_static/parse/knip_compiler.py
+++
@@ -1,8 +0,0 @@
-"""Main compiler for compiling KNIP files"""
-
-# Custom modules
-import app_static.parse.knip_tools as tools
-import error_handler
-
-class compiler:
-    """Compiles parsed tokens"""
